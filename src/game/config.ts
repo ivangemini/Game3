05e41715fddf93f3cfa3c251fa05640bbe2dfdda
ivangemini <ivangemini13@gmail.com>
2026-08-26 19:52:49 +0300

    feat: register runtime surface polish scene

--- src/game/config.ts
+++ src/game/config.ts
@@ -2,6 +2,7 @@ import * as Phaser from 'phaser';
 import { AssetPreloadScene } from './scenes/AssetPreloadScene';
 import { PrototypeScene } from './scenes/PrototypeScene';
 import { RuntimePresentationScene } from './scenes/RuntimePresentationScene';
+import { RuntimeSurfacePolishScene } from './scenes/RuntimeSurfacePolishScene';
 
 export const gameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -9,7 +10,7 @@ export const gameConfig: Phaser.Types.Core.GameConfig = {
   width: 1600,
   height: 900,
   backgroundColor: '#0b0d13',
-  scene: [AssetPreloadScene, PrototypeScene, RuntimePresentationScene],
+  scene: [AssetPreloadScene, PrototypeScene, RuntimePresentationScene, RuntimeSurfacePolishScene],
   render: {
     antialias: true,
     pixelArt: false,
