c1edf1ab6d5f006d459a3863237035b7cf1f3973
ivangemini <ivangemini13@gmail.com>
2026-08-26 21:25:23 +0300

    feat: launch compact HUD legibility layer at boot

--- src/game/scenes/AssetPreloadScene.ts
+++ src/game/scenes/AssetPreloadScene.ts
@@ -103,6 +103,7 @@ export class AssetPreloadScene extends Phaser.Scene {
     }
     this.scene.launch('runtime-presentation');
     this.scene.launch('runtime-surface-polish');
+    this.scene.launch('runtime-hud-legibility');
     this.scene.launch('runtime-flow-polish');
     this.scene.launch('runtime-archive-polish');
     this.scene.start('prototype');
